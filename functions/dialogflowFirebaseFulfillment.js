937b23b2e9e914479f29315a80071900849776a5
Olie Chan <olie.chan.141@gmail.com>
2018-07-16 23:11:49 +0100

    add fulfillment for retrieving given groups

--- functions/dialogflowFirebaseFulfillment.js
+++ functions/dialogflowFirebaseFulfillment.js
@@ -49,10 +49,28 @@ module.exports = (request, response) => {
 
     agent.add(result);
   }
+  function listInGroup(agent) {
+    const { Group } = request.body.queryResult.parameters;
+    const groups = {
+      A: "Group A: Russia, Saudi Arabia, Egypt, Uruguay",
+      B: "Group B: Portugal, Spain, Morocco, Iran",
+      C: "Group C: France, Australia, Peru, Denmark",
+      D: "Group D: Argentina, Iceland, Croatia, Nigeria",
+      E: "Group E: Brazil, Switzerland, Costa Rica, Serbia",
+      F: "Group F: Germany, Mexico, Sweden, Korea Republic",
+      G: "Group G: Belgium, Panama, Tunisia, England",
+      H: "Group H: Poland, Senegal, Colombia, Japan"
+    };
+    const result = groups[Group] || "I'm not sure";
+
+    agent.add(result);
+  }
+
   let intentMap = new Map();
   intentMap.set("Default Fallback Intent", fallback);
   intentMap.set("WhichStadiumsPlayed", listStadiums);
   intentMap.set("WhichTeams", listTeams);
   intentMap.set("WhoWereInThe", listStage);
+  intentMap.set("WhichCountriesInGroup", listInGroup);
   agent.handleRequest(intentMap);
 };
